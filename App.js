18a8347a4bf59b841ed9daa61ae6cb8da4b8f584
J05ue21 <orellana3566@gmail.com>
2026-04-18 16:53:17 -0600

    add: configuracion de la navegacion entre pantallas

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default function App() {
+// incluyendo las pantallas basicas de la app
+import LoginScreen from './src/screens/LoginScreen';
+import MenuScreen from './src/screens/MenuScreen';
+import OrderScreen from './src/screens/OrderScreen';
+import HistoryScreen from './src/screens/HistoryScreen';
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+//menu tipo tab (pestañas) mostrados tras pasar login
+function MainTabs(){
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator screenOptions={{ headerShown: true }}>
+      <Tab.Screen name="Menu" component={MenuScreen} options={{ title: 'Nuestro Menú' }} />
+      <Tab.Screen name="Orden" component={OrderScreen} options={{ title: 'Carrito' }} />
+      <Tab.Screen name="Historial" component={HistoryScreen} options={{ title: 'Historial de Compras' }} />
+    </Tab.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen 
+          name="Login" 
+          component={LoginScreen} 
+          options={{ headerShown: false }} 
+        />
+        <Stack.Screen 
+          name="Main" 
+          component={MainTabs} 
+          options={{ headerShown: false }} 
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
