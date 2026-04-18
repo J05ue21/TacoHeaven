b81095f9ceee6463dd211603c1230f32041874d5
J05ue21 <orellana3566@gmail.com>
2026-04-18 17:57:52 -0600

    agregar: Se agrega  el componente OrderScreen (plantilla simple)

---
+++ src/screens/OrderScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function OrderScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Pantalla para mostrar la Orden</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { 
+    flex: 1, 
+    justifyContent: 'center', 
+    alignItems: 'center' 
+}
+});
