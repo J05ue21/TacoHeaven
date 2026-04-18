b97383f53dc86d48022f52f2283dbc2228d10aa7
J05ue21 <orellana3566@gmail.com>
2026-04-18 16:15:35 -0600

    add: lista de platillos y bebidas disponibles

---
+++ src/data/menu.js
@@ -0,0 +1,24 @@
+// litado de comida y bebida disponibles para seleccionar
+
+export const menu = [
+  { id: '1', nombre: 'Tacos (orden de 4)', precio: 5.50, categoria: 'Comida', imagen: '' },
+  { id: '2', nombre: 'Enchiladas', precio: 5.00, categoria: 'Comida', imagen: '' },
+  { id: '3', nombre: 'Quesadillas grandes', precio: 7.00, categoria: 'Comida', imagen: '' },
+  { id: '4', nombre: 'Burrito (30cm)', precio: 8.00, categoria: 'Comida', imagen: '' },
+  { id: '5', nombre: 'Pozole rojo', precio: 6.00, categoria: 'Comida', imagen: '' },
+  { id: '6', nombre: 'Sopa de Tortilla (32 oz)', precio: 6.00, categoria: 'Comida', imagen: '' },
+  { id: '7', nombre: 'Torta Cubana', precio: 6.50, categoria: 'Comida', imagen: '' },
+  { id: '8', nombre: 'Lomo de Aguja a la Mexicana', precio: 8.75, categoria: 'Comida', imagen: '' },
+  { id: '9', nombre: 'Nachos con Queso y Carne (orden XL)', precio: 7.25, categoria: 'Comida', imagen: '' },
+  { id: '10', nombre: 'Alambres de Res', precio: 7.00, categoria: 'Comida', imagen: '' },
+
+  //bebidas 
+  { id: '11', nombre: 'Limonada con Hierbabuena', precio: 2.15, categoria: 'Bebida', imagen: '' },
+  { id: '12', nombre: 'Margarita de Limón', precio: 3.50, categoria: 'Bebida', imagen: '' },
+  { id: '13', nombre: 'Michelada Clásica', precio: 4.00, categoria: 'Bebida', imagen: '' },
+  { id: '14', nombre: 'Café Americano', precio: 1.50, categoria: 'Bebida', imagen: '' },
+  { id: '15', nombre: 'Horchata de Arroz', precio: 2.00, categoria: 'Bebida', imagen: '' },
+  { id: '16', nombre: 'Cerveza Nacional', precio: 2.50, categoria: 'Bebida', imagen: '' },
+  { id: '17', nombre: 'Soda (Coca-Cola)', precio: 2.00, categoria: 'Bebida', imagen: '' },
+  
+];
