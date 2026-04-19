482bdba2b8b765325c8d2d5794aa3ccef875333f
J05ue21 <orellana3566@gmail.com>
2026-04-18 20:24:31 -0600

    agregar: funcionalidad a OrdenScreen

--- App.js
+++ App.js
@@ -8,17 +8,31 @@ import LoginScreen from './src/screens/LoginScreen';
 import MenuScreen from './src/screens/MenuScreen';
 import OrderScreen from './src/screens/OrderScreen';
 import HistoryScreen from './src/screens/HistoryScreen';
+import DetailScreen from './src/screens/DetailScreen';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
+const MenuStack = createNativeStackNavigator();
+
+//navegacion de tipo Stack
+function MenuStackScreen(){
+  return(
+    <MenuStack.Navigator>
+      <Stack.Screen name="ListaMenu" component={MenuScreen} options={{ title: 'Menú TacoHeaven' }} />
+      
+      <Stack.Screen name="Detalle" component={DetailScreen} options={{ title: 'Configurar Orden' }} />
+    </MenuStack.Navigator>
+  );
+}
+
 //menu tipo tab (pestañas) mostrados tras pasar login
 function MainTabs(){
   return (
-    <Tab.Navigator screenOptions={{ headerShown: true }}>
-      <Tab.Screen name="Menu" component={MenuScreen} options={{ title: 'Nuestro Menú' }} />
-      <Tab.Screen name="Orden" component={OrderScreen} options={{ title: 'Carrito' }} />
-      <Tab.Screen name="Historial" component={HistoryScreen} options={{ title: 'Historial de Compras' }} />
+    <Tab.Navigator screenOptions={{ headerShown: false}}>
+      <Tab.Screen name="Menu" component={MenuStackScreen} options={{ title: 'Nuestro Menú' }} />
+      <Tab.Screen name="Orden" component={OrderScreen} options={{ title: 'Mi carrito' }} />
+      <Tab.Screen name="Historial" component={HistoryScreen} options={{ title: 'Historial de Pedidos' }} />
     </Tab.Navigator>
   );
 }
