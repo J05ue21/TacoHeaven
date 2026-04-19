57674e54d290566a9cecddccb3222e5b1ec281ba
J05ue21 <orellana3566@gmail.com>
2026-04-19 01:56:06 -0600

    actualizar : se añaden url para obtener imagenes de platillo/bebida

--- src/data/menu.js
+++ src/data/menu.js
@@ -1,24 +1,24 @@
 // litado de comida y bebida disponibles para seleccionar
 
 export const menu = [
-  { id: '1', nombre: 'Tacos (orden de 4)', precio: 5.50, categoria: 'Comida', imagen: '' },
-  { id: '2', nombre: 'Enchiladas', precio: 5.00, categoria: 'Comida', imagen: '' },
-  { id: '3', nombre: 'Quesadillas grandes', precio: 7.00, categoria: 'Comida', imagen: '' },
-  { id: '4', nombre: 'Burrito (30cm)', precio: 8.00, categoria: 'Comida', imagen: '' },
-  { id: '5', nombre: 'Pozole rojo', precio: 6.00, categoria: 'Comida', imagen: '' },
-  { id: '6', nombre: 'Sopa de Tortilla (32 oz)', precio: 6.00, categoria: 'Comida', imagen: '' },
-  { id: '7', nombre: 'Torta Cubana', precio: 6.50, categoria: 'Comida', imagen: '' },
-  { id: '8', nombre: 'Lomo de Aguja a la Mexicana', precio: 8.75, categoria: 'Comida', imagen: '' },
-  { id: '9', nombre: 'Nachos con Queso y Carne (orden XL)', precio: 7.25, categoria: 'Comida', imagen: '' },
-  { id: '10', nombre: 'Alambres de Res', precio: 7.00, categoria: 'Comida', imagen: '' },
+  { id: '1', nombre: 'Tacos (orden de 4)', precio: 5.50, categoria: 'Comida', imagen: 'https://static.excelenciasgourmet.com/cdn/ff/8HXaaAMXZJ_clZ8SQTTrY87qNfwdc343V_WwlxCOPrc/1774890339/public/articulos/2026-03/dia-mundial-taco.png' },
+  { id: '2', nombre: 'Enchiladas', precio: 5.00, categoria: 'Comida', imagen: 'https://saboresdemicocina.com/wp-content/uploads/2025/10/22queretaro-enchiladas-queretanas-68ed60c10ad5a-1.webp' },
+  { id: '3', nombre: 'Quesadillas grandes', precio: 7.00, categoria: 'Comida', imagen: 'https://i.blogs.es/2a00ea/quesadilla_vertical/450_1000.jpg' },
+  { id: '4', nombre: 'Burrito (30cm)', precio: 8.00, categoria: 'Comida', imagen: 'https://www.shutterstock.com/image-photo/mexican-beef-burrito-isolated-on-600nw-2646397505.jpg' },
+  { id: '5', nombre: 'Pozole rojo', precio: 6.00, categoria: 'Comida', imagen: 'https://static.wixstatic.com/media/d6fe55_16179d1f19314cf2b8f9d2a892edf3de~mv2.png/v1/fill/w_568,h_568,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/d6fe55_16179d1f19314cf2b8f9d2a892edf3de~mv2.png' },
+  { id: '6', nombre: 'Sopa de Tortilla (32 oz)', precio: 6.00, categoria: 'Comida', imagen: 'https://www.unileverfoodsolutions.com.mx/dam/global-ufs/mcos/NOLA/calcmenu/recipes/MX-recipes/general/sopa-de-tortilla/main-header.jpg' },
+  { id: '7', nombre: 'Torta Cubana', precio: 6.50, categoria: 'Comida', imagen: 'https://upload.wikimedia.org/wikipedia/commons/b/bb/Torta_Cubana.jpg' },
+  { id: '8', nombre: 'Lomo de Aguja a la Mexicana', precio: 8.75, categoria: 'Comida', imagen: 'https://www.tresjotas.com/wp-content/uploads/2020/12/Receta-Lomo-de-aguja-a-la-parrilla.jpg' },
+  { id: '9', nombre: 'Nachos con Queso y Carne (orden XL)', precio: 7.25, categoria: 'Comida', imagen: 'https://www.cocinavital.mx/wp-content/uploads/2017/11/nachos-con-carne-molida.jpg' },
+  { id: '10', nombre: 'Alambres de Res', precio: 7.00, categoria: 'Comida', imagen: 'https://editorialtelevisa.brightspotcdn.com/dims4/default/76f811b/2147483647/strip/true/crop/996x560+2+0/resize/1440x810!/quality/90/?url=https%3A%2F%2Fk2-prod-editorial-televisa.s3.us-east-1.amazonaws.com%2Fbrightspot%2Fwp-content%2Fuploads%2F2019%2F05%2FAlambre.jpg' },
 
   //bebidas 
-  { id: '11', nombre: 'Limonada con Hierbabuena', precio: 2.15, categoria: 'Bebida', imagen: '' },
-  { id: '12', nombre: 'Margarita de Limón', precio: 3.50, categoria: 'Bebida', imagen: '' },
-  { id: '13', nombre: 'Michelada Clásica', precio: 4.00, categoria: 'Bebida', imagen: '' },
-  { id: '14', nombre: 'Café Americano', precio: 1.50, categoria: 'Bebida', imagen: '' },
-  { id: '15', nombre: 'Horchata de Arroz', precio: 2.00, categoria: 'Bebida', imagen: '' },
-  { id: '16', nombre: 'Cerveza Nacional', precio: 2.50, categoria: 'Bebida', imagen: '' },
-  { id: '17', nombre: 'Soda (Coca-Cola)', precio: 2.00, categoria: 'Bebida', imagen: '' },
+  { id: '11', nombre: 'Limonada con Hierbabuena', precio: 2.15, categoria: 'Bebida', imagen: 'https://www.hola.com/horizon/landscape/cd43e77c4a2f-receta-limonada-hierbabuena-limon-t.jpg' },
+  { id: '12', nombre: 'Margarita de Limón', precio: 3.50, categoria: 'Bebida', imagen: 'https://cocina-casera.com/mx/wp-content/uploads/2018/04/preparar-margaritas.jpg' },
+  { id: '13', nombre: 'Michelada Clásica', precio: 4.00, categoria: 'Bebida', imagen: 'https://cdn7.kiwilimon.com/brightcove/6390/640x640/6390.jpg.webp' },
+  { id: '14', nombre: 'Café Americano', precio: 1.50, categoria: 'Bebida', imagen: 'https://excelso77.com/wp-content/uploads/2024/05/por-que-el-cafe-americano-se-llama-asi-te-lo-contamos.webp' },
+  { id: '15', nombre: 'Horchata de Arroz', precio: 2.00, categoria: 'Bebida', imagen: 'https://mylatinatable.com/wp-content/uploads/2014/01/foto-4-1024x832.jpg' },
+  { id: '16', nombre: 'Cerveza Nacional', precio: 2.50, categoria: 'Bebida', imagen: 'https://i.pinimg.com/474x/11/f7/0d/11f70d1ba0a25efd80599e6992bc9a52.jpg' },
+  { id: '17', nombre: 'Soda (Coca-Cola)', precio: 2.00, categoria: 'Bebida', imagen: 'https://st2.depositphotos.com/4431055/11855/i/950/depositphotos_118552742-stock-photo-coca-cola-cansglass-and-bottles.jpg' },
   
 ];
